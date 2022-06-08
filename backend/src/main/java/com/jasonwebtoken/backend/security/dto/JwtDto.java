bbfbadc12427f8873083cb68666355070939330c
Danilo Maccari <maccari78@gmail.com>
2022-06-08 09:44:24 +0100

    JWT OK

--- backend/src/main/java/com/jasonwebtoken/backend/security/dto/JwtDto.java
+++ backend/src/main/java/com/jasonwebtoken/backend/security/dto/JwtDto.java
@@ -1,19 +1,14 @@
 package com.jasonwebtoken.backend.security.dto;
 
-import java.util.Collection;
-import org.springframework.security.core.GrantedAuthority;
-
 public class JwtDto {
+
+    public JwtDto() {
+    }
     
     private String token;
-    private String bearer = "Bearer";
-    private String nombreUsuario;
-    private Collection<? extends GrantedAuthority> authorities;
-
-    public JwtDto(String token, String nombreUsuario, Collection<? extends GrantedAuthority> authorities) {
+    
+    public JwtDto(String token) {
         this.token = token;
-        this.nombreUsuario = nombreUsuario;
-        this.authorities = authorities;
     }
 
     public String getToken() {
@@ -24,30 +19,5 @@ public class JwtDto {
         this.token = token;
     }
 
-    public String getBearer() {
-        return bearer;
-    }
-
-    public void setBearer(String bearer) {
-        this.bearer = bearer;
-    }
-
-    public String getNombreUsuario() {
-        return nombreUsuario;
-    }
-
-    public void setNombreUsuario(String nombreUsuario) {
-        this.nombreUsuario = nombreUsuario;
-    }
-
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
-    }
-
-    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
-        this.authorities = authorities;
-    }
-    
-    
-    
+        
 }
