42a1368e2806dd776a342c0e6fd95c7dd192dcfa
Danilo Maccari <maccari78@gmail.com>
2022-06-23 10:03:38 +0100

    end of 5

--- backend/src/main/java/com/jasonwebtoken/backend/forgotpassword/controller/EmailController.java
+++ backend/src/main/java/com/jasonwebtoken/backend/forgotpassword/controller/EmailController.java
@@ -3,6 +3,9 @@ package com.jasonwebtoken.backend.forgotpassword.controller;
 import com.jasonwebtoken.backend.dto.Mensaje;
 import com.jasonwebtoken.backend.forgotpassword.dto.EmailValuesDTO;
 import com.jasonwebtoken.backend.forgotpassword.service.EmailService;
+import com.jasonwebtoken.backend.security.entity.Usuario;
+import com.jasonwebtoken.backend.security.service.UsuarioService;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,18 +23,30 @@ public class EmailController {
     @Autowired
     EmailService emailService;
     
+    @Autowired
+    UsuarioService usuarioService;
+    
     @Value("${spring.mail.username}")
     private String mailFrom;
     
+    private static final String subject = "Cambio de Contraseña";
+    
     @PostMapping("/send-email")
-    public ResponseEntity<?> sendEmailTemplate(@RequestBody EmailValuesDTO dto){
+    public ResponseEntity<?> sendEmailTemplate(@RequestBody EmailValuesDTO dto) {
+        Optional<Usuario> usuarioOpt = usuarioService.getByNombreUsuarioOrEmail(dto.getMailTo());
+        if(!usuarioOpt.isPresent())
+            return new ResponseEntity(new Mensaje("No existe ningún usuario con esas credenciales"), HttpStatus.NOT_FOUND);
+        Usuario usuario = usuarioOpt.get();
         dto.setMailFrom(mailFrom);
-        dto.setSubject("Cambio de contraseña");
-        dto.setUserName("Danilo");
-        UUID uuid=UUID.randomUUID();
-        String forgotPassword=uuid.toString();
-        dto.setForgotPassword(forgotPassword);
+        dto.setMailTo(usuario.getEmail());
+        dto.setSubject(subject);
+        dto.setUserName(usuario.getNombreUsuario());
+        UUID uuid = UUID.randomUUID();
+        String tokenPassword = uuid.toString();
+        dto.setForgotPassword(tokenPassword);
+        usuario.setForgotPassword(tokenPassword);
+        usuarioService.save(usuario);
         emailService.sendEmail(dto);
         return new ResponseEntity(new Mensaje("Te hemos enviado un correo"), HttpStatus.OK);
-    } 
+    }
 }
